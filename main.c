7acb4685eb7b04ef1e19ec207432ff14ea432acd
unknown <kamelm9400@gmail.com>
2020-09-20 07:58:51 -1100

    a

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float x, c = 15.72;
+
+    printf("Hello!\n Enter the number in ($)");
+    scanf("%f",&x );
+
+    float z = x * c;
+    printf("your number in (E G P) is %f",z);
+
+    return 0;
+}
